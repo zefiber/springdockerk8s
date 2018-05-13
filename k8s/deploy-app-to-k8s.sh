2ead58cd8321627b0e2f39ffa8638b742c616260
Ze Wang <Ze.Wang@pointclickcare.com>
2018-05-12 22:31:14 -0400

    change to shell script

--- k8s/deploy-app-to-k8s.sh
+++ k8s/deploy-app-to-k8s.sh
@@ -1,3 +1,3 @@
 kubectl delete deployment springdockerk8s-deployment
 kubectl delete service springdocker-service
-kubectl create -f springdocker-deployment.yaml
+kubectl create -f springdocker-deployment.yaml > start-app.log
