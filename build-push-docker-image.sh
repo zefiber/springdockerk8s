8edde80dcaec106d347b1e6111c623d59f74104c
Ze Wang <Ze.Wang@pointclickcare.com>
2018-05-13 10:12:41 -0400

    change the project artifact name

--- build-push-docker-image.sh
+++ build-push-docker-image.sh
@@ -1,3 +1,3 @@
 mvn dockerfile:build
-docker tag springdockerk8s:0.0.1 zefiber/zetest:springdockerfork8s_v5
+docker tag springdockerk8sdemo:0.0.1 zefiber/zetest:springdockerfork8s_v5
 docker push zefiber/zetest:springdockerfork8s_v5
