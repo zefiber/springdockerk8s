d900c0d98ace51b5debe1aad58c25ea1d7ea3f56
Ze Wang <Ze.Wang@pointclickcare.com>
2018-05-14 13:13:34 -0400

    Update the script to make it run only all in one.

--- build-push-deploy.sh
+++ build-push-deploy.sh
@@ -1,8 +1,24 @@
-mvn -DskipTests clean package dockerfile:build
+#!/bin/bash
+version="$1"
 
-docker tag springdockerk8sdemo:0.0.1 zefiber/springdockerk8sdemo:0.0.5
-docker push zefiber/springdockerk8sdemo:0.0.5
+if [ -z "$1"] ;then
+  mvn --batch-mode release:update-versions
+  mvn -DskipTests clean package dockerfile:build
+  export version=$(mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]')
+fi
+
+if [ "$version" = "" ] ;then
+  version="0.0.7"
+fi
+
+echo zefiber/springdockerk8sdemo:$version
+
+
+
+docker tag springdockerk8sdemo:$version zefiber/springdockerk8sdemo:$version
+docker push zefiber/springdockerk8sdemo:$version
 
 kubectl delete deployment springdockerk8s-deployment
 kubectl delete service springdocker-service
 kubectl create -f springdocker-deployment.yaml
+kubectl set image deployment/springdockerk8s-deployment springdockerk8s=zefiber/springdockerk8sdemo:$version
--- k8s/deploy-app-to-k8s.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -ex
-kubectl delete deployment springdockerk8s-deployment
-kubectl delete service springdocker-service
-kubectl create -f springdocker-deployment.yaml
